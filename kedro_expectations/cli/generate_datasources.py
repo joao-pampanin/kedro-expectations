c8d997165bdc9ce1349e8449694cb0997171fa39
joao-pampanin <joao.pampanin@advisia.com>
2022-11-18 23:54:25 +0000

    refactoring in order to use node inputs for the expectations validations

--- kedro_expectations/cli/generate_datasources.py
+++ kedro_expectations/cli/generate_datasources.py
@@ -1,66 +1,23 @@
 import click
-import os, yaml
 import great_expectations as ge
-from great_expectations.cli.datasource import sanitize_yaml_and_save_datasource
-from great_expectations import execution_engine
-from great_expectations.exceptions.exceptions import ConfigNotFoundError
-from kedro_expectations.utils import get_execution_engine_class
 
 @click.command()
-def generate_datasources():
-    
-
-
-    current_dir_path = os.getcwd()
-    catalog_path = os.path.join(current_dir_path, "conf", "base", "catalog.yml")
-
+def generate_generic_datasource():
+    # TODO - Ver o que acontece se eu rodar isso 2x no mesmo projeto
     context = ge.get_context()
-
-    with open(catalog_path, "r") as stream:
-        try:
-            created_datasources = []
-            catalog = yaml.safe_load(stream)
-            for catalog_item in catalog:
-                dataset = catalog[catalog_item]
-                parent_path, filename = os.path.split(dataset['filepath']) # data/01_raw, companies
-                filename, fileextension = os.path.splitext(filename) # companies, .csv
-                dataset_type = dataset['type'] # pandas.CSVDataSet
-                execution_engine_class, execution_engine_aux_name = get_execution_engine_class(dataset, fileextension)
-                
-                if execution_engine_class == None:
-                    print("The dataset ", catalog_item, " is currently not supported by Kedro Expectations! A datasource will not be created for this dataset")
-                    print("A datasource may still be created if there are supported datasets in this same folder, but they should not be used with ", catalog_item)
-                    continue
-                
-                # TODO - Em vez de yaml, da pra usar uma maneira mais confiável (ver na doc do GE)
-                datasource_name = parent_path + "_" + execution_engine_aux_name + "_gedatasource"
-                if datasource_name not in created_datasources:
-                    example_yaml = f"""
-                    name: {datasource_name}
-                    class_name: Datasource
-                    execution_engine:
-                        class_name: {execution_engine_class}
-                    data_connectors:
-                        default_inferred_data_connector_name:
-                            class_name: InferredAssetFilesystemDataConnector
-                            base_directory: ../{parent_path}
-                            default_regex:
-                                group_names:
-                                    - data_asset_name
-                                pattern: (.*)
-                        default_runtime_data_connector_name:
-                            class_name: RuntimeDataConnector
-                            assets:
-                                my_runtime_asset_name:
-                                    batch_identifiers:
-                                    - runtime_batch_identifier_name
-                    """
-                    # TODO - Criar exception para esse teste?
-                    #context.test_yaml_config(yaml_config=example_yaml)
-                    sanitize_yaml_and_save_datasource(context, example_yaml, overwrite_existing=False)
-
-                    created_datasources.append(datasource_name)
-            context.list_datasources()
-
-        except yaml.YAMLError as exc:
-            print("Error while parsing YAML:\n", exc)
+    datasource_config = {
+        "name": "generic_datasource",
+        "class_name": "Datasource",
+        "module_name": "great_expectations.datasource",
+        "execution_engine": {
+            "module_name": "great_expectations.execution_engine",
+            "class_name": "PandasExecutionEngine",
+        },
+        "data_connectors": {
+            "default_runtime_data_connector_name": {
+                "class_name": "RuntimeDataConnector",
+                "batch_identifiers": ["default_identifier_name"],
+            },
+        },
+    }
+    context.add_datasource(**datasource_config)
--- kedro_expectations/kedro_expectations.py
+++ kedro_expectations/kedro_expectations.py
@@ -1,12 +1,18 @@
-import os, yaml
 from typing import Any, Dict
-
+import pandas as pd
 import great_expectations as ge
 from kedro.framework.hooks import hook_impl
 
-from ruamel.yaml import YAML
 import datetime
-from kedro_expectations.utils import get_execution_engine_class
+from great_expectations.exceptions import DataContextError
+
+import datetime
+
+import pandas as pd
+
+import great_expectations as ge
+from great_expectations.exceptions import DataContextError
+
 
 class KedroExpectationsHooks:
     def __init__(self) -> None:
@@ -19,56 +25,39 @@ class KedroExpectationsHooks:
 
 
     def _run_validation(self, data: Dict[str, Any]):
-        ruamel_yaml = YAML()
-        context = ge.get_context()
+        for key, value in data.items():
+            try:
+                if isinstance(value, pd.DataFrame):
+                    context = ge.get_context()
+                    formatted_time = datetime.datetime.now(datetime.timezone.utc).strftime("%Y%m%dT%H%M%S")
 
-        # TODO - Esse código de catálogo + pegar file_extension provavelmente é desnecessário
+                    checkpoint_config = {
+                        "name": "my_missing_keys_checkpoint",
+                        "config_version": 1,
+                        "class_name": "SimpleCheckpoint",
+                        "validations": [
+                            {
+                                "batch_request": {
+                                    "datasource_name": "generic_datasource",
+                                    "data_connector_name": "default_runtime_data_connector_name",
+                                    "data_asset_name": key,
+                                },
+                                "expectation_suite_name": key + ".myexp",
+                            }
+                        ],
+                    }
+                    context.add_checkpoint(**checkpoint_config)
 
-        current_dir_path = os.getcwd()
-        catalog_path = os.path.join(current_dir_path, "conf", "base", "catalog.yml")
-        for catalog_item in data:
-            formatted_time = datetime.datetime.now(datetime.timezone.utc).strftime("%Y%m%dT%H%M%S")
-            my_checkpoint_name = str(catalog_item) + "_checkpoint_" + str(formatted_time)
-
-            # TODO - Como atuar quando existir mais de uma suite para um mesmo dataset?
-            
-            with open(catalog_path, "r") as stream:
-                try:
-                    catalog = yaml.safe_load(stream)
-                    dataset = catalog[catalog_item]
-                    parent_path, filename = os.path.split(dataset['filepath']) # data/01_raw, companies.csv
-                    filename, fileextension = os.path.splitext(filename) # companies, .csv
-                    execution_engine_class, execution_engine_aux_name = get_execution_engine_class(dataset, fileextension)
-                    datasource_name = parent_path + "_" + str(execution_engine_aux_name) + "_gedatasource"
-                    if execution_engine_class != None:
-                        yaml_config = f"""
-                        name: {my_checkpoint_name}
-                        config_version: 1.0
-                        class_name: SimpleCheckpoint
-                        run_name_template: Kedro-Great-Run-{formatted_time}
-                        validations:
-                          - batch_request:
-                              datasource_name: {datasource_name}
-                              data_connector_name: default_inferred_data_connector_name
-                              data_asset_name: {catalog_item}{fileextension}
-                              data_connector_query:
-                                index: -1
-                            expectation_suite_name: {catalog_item}.myexp
-                        """
-
-                        # TODO - Exception pra esse teste
-                        my_checkpoint = context.test_yaml_config(yaml_config=yaml_config)
-
-                        # print(my_checkpoint.get_config(mode="yaml"))
-
-                        context.add_checkpoint(**ruamel_yaml.load(yaml_config))
-
-                        context.run_checkpoint(checkpoint_name=my_checkpoint_name)
-                    else:
-                        print(f"The expectation suite \"{catalog_item}.myexp\" was not found, so Kedro Expectations will skip the validation for this datasource")
-                except yaml.YAMLError as exc:
-                    print("Error while parsing YAML:\n", exc)
-                    continue
-                except KeyError as key:
-                    print(f"The item {catalog_item} is not present in the Data Catalog, so naturally its validation will be skipped")
-                    continue
+                    results = context.run_checkpoint(
+                        run_name="Kedro-Expectations-Run"+formatted_time,
+                        checkpoint_name="my_missing_keys_checkpoint",
+                        batch_request={
+                            "runtime_parameters": {"batch_data": value},
+                            "batch_identifiers": {
+                                "default_identifier_name": "default_identifier"
+                            },
+                        },
+                    )
+            except DataContextError:
+                print(f"No expectation suite was found for \"{key}\", so Kedro Expectations will skip the validation for this datasource")
+                continue
